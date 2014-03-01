0a802598a291b8f6a2fd5fb51437d90b6ec41aa0
Bert Muthalaly <bgmuthalaly@gmail.com>
2014-02-28 20:52:12 -0600

    PKTEventManager openIntervals method working!

--- PacktTests/PacktTests.m
+++ PacktTests/PacktTests.m
@@ -7,9 +7,12 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <EventKit/EventKit.h>
+#import "PKTEventManager.h"
 
 @interface PacktTests : XCTestCase
-
+@property PKTEventManager *eventManager;
+@property NSArray *events;
 @end
 
 @implementation PacktTests
@@ -18,6 +21,22 @@
 {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.eventManager = [[PKTEventManager alloc] init];
+    // Add EKEvent to calendar
+    EKEventStore *store = [[EKEventStore alloc] init];
+    EKEvent *event1 = [EKEvent eventWithEventStore:store];
+    EKEvent *event2 = [EKEvent eventWithEventStore:store];
+    event1.startDate = [[NSDate alloc] init];
+    NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    NSDateComponents *offsetComponents = [[NSDateComponents alloc] init];
+    [offsetComponents setHour:1];
+    [offsetComponents setMinute:30];
+    event1.endDate = [gregorian dateByAddingComponents:offsetComponents
+                                                toDate:event1.startDate options:0];
+    // make event 2 start 1:30 after event 1
+    event2.startDate = [gregorian dateByAddingComponents:offsetComponents toDate:event1.endDate options:0];
+    event2.endDate = [gregorian dateByAddingComponents:offsetComponents toDate:event2.startDate options:0];
+    self.events = @[event1, event2];
 }
 
 - (void)tearDown
@@ -28,7 +47,9 @@
 
 - (void)testExample
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    NSArray *intervals = [self.eventManager openTimeIntervalsBetweenEventsInArray:self.events];
+    NSNumber *correctInterval = [NSNumber numberWithDouble:60*60*1.5];
+    XCTAssertEqualObjects(intervals, @[correctInterval], "Got interval array %@ but expected array %@");
 }
 
 @end
