cdd92acef3afc5ccb70597f08e813351b6532abf
Bert Muthalaly <bgmuthalaly@gmail.com>
2014-03-01 09:54:23 -0600

    DynamicAnimator is working. (It’s providing the stickiness of the Tasks collectionView when scrolling)

--- Packt/PKTTaskCollectionLayout.m
+++ Packt/PKTTaskCollectionLayout.m
@@ -20,6 +20,18 @@
     CGSize contentSize = self.collectionView.contentSize;
     NSArray *items = [super layoutAttributesForElementsInRect:
                       CGRectMake(0.0f, 0.0f, contentSize.width, contentSize.height)];
+    if (self.dynamicAnimator.behaviors.count == 0) {
+        [items enumerateObjectsUsingBlock:^(id<UIDynamicItem> obj, NSUInteger idx, BOOL *stop) {
+            UIAttachmentBehavior *behaviour = [[UIAttachmentBehavior alloc] initWithItem:obj
+                                                                        attachedToAnchor:[obj center]];
+            
+            behaviour.length = 0.0f;
+            behaviour.damping = 0.8f;
+            behaviour.frequency = 1.0f;
+            
+            [self.dynamicAnimator addBehavior:behaviour];
+        }];
+    }
 }
 
 
