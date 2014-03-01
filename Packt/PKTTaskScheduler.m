a88af157c57aa69a7df3bef22d5c35be356108f1
Bert Muthalaly <bgmuthalaly@gmail.com>
2014-02-28 23:35:39 -0600

    Start FFD bin packing implementation

--- Packt/PKTTaskScheduler.m
+++ Packt/PKTTaskScheduler.m
@@ -7,7 +7,29 @@
 //
 
 #import "PKTTaskScheduler.h"
+#import "PKTInterval.h"
 
 @implementation PKTTaskScheduler
 
+- (NSArray *) firstFitDecreasingPackingOfTasks:(NSArray *)tasks intoIntervals:(NSArray *)intervals
+{
+    // sort tasks into decreasing order
+    NSArray *sortedTasks = [tasks sortedArrayUsingSelector:NSSelectorFromString(@"interval")];
+    NSDictionary *intervalsToRemainingTime = [self doubleKeyedDictionaryFromIntervalArray:intervals];
+    for(PKTTask *task in tasks.reverseObjectEnumerator) {
+        for (PKTInterval *interval in intervals) {
+            // attempt to place task in interval
+            if()
+        }
+    }
+}
+
+- (NSDictionary *) doubleKeyedDictionaryFromIntervalArray:(NSArray *)array
+{
+    NSMutableDictionary *mutableDictionary = [[NSMutableDictionary alloc] init];
+    for (PKTInterval *intervalInstance in array)
+        [mutableDictionary setObject:[NSNumber numberWithDouble:[intervalInstance interval]]
+                              forKey: intervalInstance];
+    return (NSDictionary *)mutableDictionary;
+}
 @end
